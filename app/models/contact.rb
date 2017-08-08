294d76d944f5d5e78af9259cd8193aad574a6ca8
Nogueira <anogueira@ionatec.com.br>
2017-08-08 20:23:13 -0300

    Optimal kind

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,19 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind#, optional: true
+
+  # def author
+  #   "Antonio Carlos"
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :author]
+  #     #include: :kind
+  #   )
+  # end
 end
