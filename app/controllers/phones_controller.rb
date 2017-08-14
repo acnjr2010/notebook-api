de711927d460f6513bfb823aed65c64067b052ba
Nogueira <anogueira@ionatec.com.br>
2017-08-14 20:51:53 -0300

    Rotas dentro do padrão usando o has_many e json

---
+++ app/controllers/phones_controller.rb
@@ -0,0 +1,19 @@
+class PhonesController < ApplicationController
+  before_action :set_phones
+
+  # GET /phones/1
+  def show
+    render json: @phones
+  end
+
+  private
+
+  def set_phones
+    if params[:contact_id]
+      @phones = Contact.find(params[:contact_id]).phones
+      return @phones
+    end
+
+    @phones = phones.where(id: params[:id])
+  end
+end
